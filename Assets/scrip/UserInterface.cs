ca1f009c1e5536b211079f03c7f431c2072b25bf
Roswell_Winters <alessandra.l.wint@gmail.com>
2024-07-16 22:14:57 -0500

    added menu press function to UI script
    need to test this script tomorrow at work

--- Assets/scrip/UserInterface.cs
+++ Assets/scrip/UserInterface.cs
@@ -1,9 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.OpenXR.Input;
+using UnityEngine.InputSystem;
 
 public class UserInterface : MonoBehaviour
 {
+    private UnityEngine.XR.InputDevice leftHandDevice;
+    private bool menuButtonPressed;
+
+    void Start()
+    {
+        leftHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+    }
+    void Update()
+    {
+        // Check if the right hand device is valid
+        if (leftHandDevice.isValid)
+        {
+            bool pressed = false;
+
+            // Check if the menu button is pressed
+            if (leftHandDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.menuButton, out pressed) && pressed)
+            {
+                if (!menuButtonPressed)
+                {
+                    menuButtonPressed = true;
+                    OnMenuButtonPressed();
+                }
+            }
+            else
+            {
+                if (menuButtonPressed)
+                {
+                    menuButtonPressed = false;
+                }
+            }
+        }
+    }
+
+    void OnMenuButtonPressed()
+    {
+
+    }
+
     public void QuitGame()
     {
         //will only work in a build
