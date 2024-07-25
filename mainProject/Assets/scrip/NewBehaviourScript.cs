462ffa4dad90987c8defdd5d45d60cead3f5702c
Tre <rodneywhitneyiii@gmail.com>
2024-07-25 09:32:06 -0500

    bro am i stupid

---
+++ mainProject/Assets/scrip/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.game
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit()
+    {
+       this.gameObject 
+    }
+}
