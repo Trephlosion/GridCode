18f8b83b52688a5d9cc6b458ff85ba6ea769bb26
Tre <rodneywhitneyiii@gmail.com>
2024-07-26 15:58:56 -0500

    new details

--- mainProject/Assets/scrip/Respawn.cs
+++ mainProject/Assets/scrip/Respawn.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Respawn : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        this.game
+        
     }
 
     // Update is called once per frame
-    void OnTriggerExit()
+    void Update()
     {
-       this.gameObject 
+        
     }
 }
